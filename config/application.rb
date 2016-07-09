dfb0ffcd03ed3270fe8b7a09bda827cccd42b3bf
Dan Uber <djuber@gmail.com>
2016-07-09 14:28:56 -0500

    initial checkin

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Wpfiles
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.web_console.whitelisted_ips = '216.80.113.156'
+  end
+end
