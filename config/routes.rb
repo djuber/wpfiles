a36675db9c10b54a84c24929bfecf5564971dd42
Dan Uber <djuber@gmail.com>
2016-07-09 14:45:38 -0500

    nesting files inside versions - route only

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :core_files
-  resources :wordpresses, path: "wordpress"
+
+  resources :wordpresses, path: "wordpress" do
+    resources :core_files
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
